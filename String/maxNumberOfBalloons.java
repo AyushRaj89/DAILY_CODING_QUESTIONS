b9b2d4a182289270c9b1afde2f69274ae363b543
Ayush Raj <151430844+AyushRaj89@users.noreply.github.com>
2026-07-04 07:36:36 +0530

    Implement maxNumberOfBalloons method

---
+++ String/maxNumberOfBalloons.java
@@ -0,0 +1,19 @@
+//1189. Maximum Number of Balloons
+
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int s = text.length();
+
+        int[] freq = new int[26];
+
+        for(char ch: text.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        return Math.min(
+            Math.min(freq['b'-'a'], freq['a'-'a']),
+            Math.min(
+                Math.min(freq['l'-'a']/2, freq['o'-'a']/2), freq['n'-'a'])
+            );
+    }
+}
