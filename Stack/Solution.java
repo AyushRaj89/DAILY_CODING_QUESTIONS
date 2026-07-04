fb5aa735bed959706e25cacc2de19ecb44e1a6f9
Ayush Raj <151430844+AyushRaj89@users.noreply.github.com>
2026-07-04 07:33:46 +0530

    Add initial structure for Valid Parentheses solution

---
+++ Stack/Solution.java
@@ -0,0 +1,25 @@
+// 20. Valid Parentheses
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else if(ch == ')' || ch == '}' || ch == ']'){
+                if(st.isEmpty()) {
+                    return false;
+                }
+                char top = st.peek();
+                if((ch == ')' && top != '(')
+                || (ch == '}' && top != '{')
+                || (ch == ']' && top != '[')) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
