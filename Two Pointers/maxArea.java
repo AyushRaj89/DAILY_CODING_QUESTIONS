e6edfd08ba895001513abdb722286941bd036d95
Ayush Raj <151430844+AyushRaj89@users.noreply.github.com>
2026-07-14 14:37:51 +0530

    Add maxArea.java for Container With Most Water problem

---
+++ Two Pointers/maxArea.java
@@ -0,0 +1,25 @@
+//11. Container With Most Water
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+
+       int maxArea = 0;
+
+       while(left < right) {
+        int width = right - left;
+        int currArea = width * Math.min(height[left], height[right]);
+
+        maxArea = Math.max(currArea, maxArea);
+
+        if(height[left] < height[right]) {
+            left++;
+        }else{
+            right--;
+        }
+       }
+
+       return maxArea;
+    }
+}
